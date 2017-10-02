7fb088a9e0d1a5ee2a5d88a16e10fc6e726d9bab
darya-barkouskaya <dashabarkovskaya26@gmail.com>
2017-10-02 11:45:20 +0300

    Done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.low = 0;
+    	this.middle = 0;
+    	this.high = 0;
+    }
 
     setRange(min, max) {
-
+    	this.low= min;
+    	this.high = max;
     }
 
     guess() {
-
+    	this.middle = Math.ceil((this.low + this.high)/2);
+    	return this.middle;
     }
 
     lower() {
-
+    	this.high = this.middle;
     }
 
     greater() {
-
+    	this.low =this.middle;
     }
 }
 
